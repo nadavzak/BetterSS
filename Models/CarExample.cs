780f8d6eedb41effd0c22d723070dc6e93782679
nadavzak <57315216+nadavzak@users.noreply.github.com>
2020-03-11 18:01:44 +0200

    Add files via upload

---
+++ Models/CarExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetterSS.Models
+{
+    public class CarExample
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double Price { get; set; }
+
+
+        public int insert() {
+            DBservices dbs = new DBservices();
+            int numAffected = dbs.insert(this);
+            return numAffected;
+        }
+    }
+}
